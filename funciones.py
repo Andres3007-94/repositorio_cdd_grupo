c46759822d10e16f1dd4660fbed3811081a35bed
Andres <axelx_9@hotmail.com>
2026-02-13 20:25:56 -0500

    se agrega funcion de multiplicar y mi archivo main_py

--- funciones.py
+++ funciones.py
@@ -3,5 +3,9 @@ def sumar(a,b):
 def resta(a,b):
     return a-b
 
-print(f"la suma es{ sumar(3,8) }")
+def multiplicar(a,b):
+    return a*b
+
+print(f"la suma es { sumar(3,8) }")
 print( resta(11,4))
+print( multiplicar(11,4))
